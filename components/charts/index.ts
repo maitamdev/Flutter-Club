4416d497f42b3fa354ed39853736f81cc9139204
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:02:11 +0700

    feat(charts): add barrel export for chart components

---
+++ components/charts/index.ts
@@ -0,0 +1,4 @@
+﻿export { DonutChart } from './donut-chart'
+export { SimpleBarChart } from './bar-chart'
+export { SimpleLineChart } from './line-chart'
+export { SimpleAreaChart } from './area-chart'
