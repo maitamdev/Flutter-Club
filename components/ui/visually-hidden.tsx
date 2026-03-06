bfb6d692062c6bcdb8f0f7a8d5a4ec0b25809a81
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:02:14 +0700

    feat(a11y): add VisuallyHidden component

---
+++ components/ui/visually-hidden.tsx
@@ -0,0 +1,4 @@
+﻿interface VisuallyHiddenProps { children: React.ReactNode }
+export function VisuallyHidden({ children }: VisuallyHiddenProps) {
+  return <span className="sr-only">{children}</span>
+}
