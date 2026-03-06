a19d6986199b12d091afae060499ca68b108778a
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:02:30 +0700

    feat(types): add realtime subscription types

---
+++ types/realtime.ts
@@ -0,0 +1,10 @@
+﻿// Realtime subscription types
+export type UnsubscribeFn = () => void
+
+export interface RealtimeConfig {
+  collection: string
+  orderBy?: string
+  orderDirection?: 'asc' | 'desc'
+  limitTo?: number
+  where?: { field: string; op: string; value: unknown }[]
+}
