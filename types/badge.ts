c0ce1cda3cc2727f4dc4dcf30db28380b08a12e2
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:02:29 +0700

    feat(types): add badge and user stats types

---
+++ types/badge.ts
@@ -0,0 +1,14 @@
+﻿// Badge and achievement types
+export interface UserBadge { badgeId: string; earnedAt: Date }
+export interface UserStats {
+  totalCheckins: number
+  consecutiveCheckins: number
+  totalSubmissions: number
+  submissionRate: number
+  averageScore: number
+  quizzesTaken: number
+  perfectQuizStreak: number
+  earlySubmissions: number
+  helpfulComments: number
+  isTopScorer: boolean
+}
