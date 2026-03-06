7ace88fce111285edcb36d7f9731c4eda66d0090
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:01:39 +0700

    feat(constants): add app metadata constants

---
+++ lib/constants/app.ts
@@ -0,0 +1,4 @@
+﻿// App-wide constants
+export const APP_NAME = 'WebOOM DHV TEC'
+export const APP_DESCRIPTION = 'He thong quan ly Cau lac bo Flutter - Khoa KTCN - DH Hung Vuong TPHCM'
+export const APP_VERSION = '1.0.0'
