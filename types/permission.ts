a46dbc31db14822f3c8cad7d0bc33d470788a602
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:02:16 +0700

    feat(types): add permission enum types

---
+++ types/permission.ts
@@ -0,0 +1,19 @@
+﻿// Permission types
+export type Permission =
+  | 'manage_members'
+  | 'create_session'
+  | 'create_assignment'
+  | 'create_quiz'
+  | 'create_announcement'
+  | 'grade_submission'
+  | 'start_attendance'
+  | 'delete_content'
+  | 'approve_requests'
+  | 'view_analytics'
+  | 'export_data'
+  | 'upload_material'
+
+export interface RolePermissions {
+  role: string
+  permissions: Permission[]
+}
