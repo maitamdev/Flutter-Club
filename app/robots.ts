1f4cf48fad3ca0e1a5d08f418864c25fa2790729
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:01:58 +0700

    feat(seo): add robots.txt generation

---
+++ app/robots.ts
@@ -0,0 +1,8 @@
+﻿import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: { userAgent: '*', allow: '/', disallow: ['/api/', '/sessions/', '/assignments/', '/quizzes/', '/members/', '/access-requests/', '/profile/'] },
+    sitemap: 'https://weboom-dhv-tec.vercel.app/sitemap.xml',
+  }
+}
