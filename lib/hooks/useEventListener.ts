050f173ce2353e28a7dab17a8390662f3f558004
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:02:23 +0700

    feat(hooks): add useEventListener hook

---
+++ lib/hooks/useEventListener.ts
@@ -0,0 +1,13 @@
+﻿'use client'
+import { useRef, useEffect, useCallback } from 'react'
+
+export function useEventListener<K extends keyof WindowEventMap>(eventName: K, handler: (event: WindowEventMap[K]) => void, element?: HTMLElement | Window | null) {
+  const savedHandler = useRef(handler)
+  useEffect(() => { savedHandler.current = handler }, [handler])
+  useEffect(() => {
+    const targetElement = element || window
+    const listener = (event: Event) => savedHandler.current(event as WindowEventMap[K])
+    targetElement.addEventListener(eventName, listener)
+    return () => targetElement.removeEventListener(eventName, listener)
+  }, [eventName, element])
+}
