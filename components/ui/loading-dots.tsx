0dc726d6b5e222966d8f9edca93db086814f6f9e
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:02:21 +0700

    feat(ui): add LoadingDots animation component

---
+++ components/ui/loading-dots.tsx
@@ -0,0 +1,9 @@
+﻿export function LoadingDots({ className }: { className?: string }) {
+  return (
+    <span className={className}>
+      <span className="inline-block animate-bounce" style={{ animationDelay: '0ms' }}>.</span>
+      <span className="inline-block animate-bounce" style={{ animationDelay: '150ms' }}>.</span>
+      <span className="inline-block animate-bounce" style={{ animationDelay: '300ms' }}>.</span>
+    </span>
+  )
+}
