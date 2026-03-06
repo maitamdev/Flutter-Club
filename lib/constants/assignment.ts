ca44f21fa193394f1f3784bd0a9b4a79c17c14b2
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:01:41 +0700

    feat(constants): add assignment configuration

---
+++ lib/constants/assignment.ts
@@ -0,0 +1,7 @@
+﻿// Assignment configuration
+export const ASSIGNMENT_CONFIG = {
+  MAX_FILE_SIZE_MB: 10,
+  ALLOWED_FILE_TYPES: ['.pdf', '.doc', '.docx', '.zip', '.rar'],
+  MAX_RUBRIC_ITEMS: 20,
+  MAX_SCORE: 100,
+}
