1edd5a7c3eabddb2dbb2e177daf5bdb3acc742fb
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:02:16 +0700

    feat(types): add table column and action types

---
+++ types/table.ts
@@ -0,0 +1,15 @@
+﻿// Table column types
+export interface TableColumn<T> {
+  key: keyof T
+  label: string
+  sortable?: boolean
+  width?: string
+  render?: (value: T[keyof T], row: T) => React.ReactNode
+}
+export interface TableAction<T> {
+  label: string
+  icon?: React.ReactNode
+  onClick: (row: T) => void
+  variant?: 'default' | 'destructive'
+  show?: (row: T) => boolean
+}
