bd1a91fd4797a9018584985798fa6eac400b9dae
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:02:06 +0700

    feat(hooks): add useIntersectionObserver hook

---
+++ lib/hooks/useIntersectionObserver.ts
@@ -0,0 +1,14 @@
+﻿'use client'
+import { useEffect, useRef, useState } from 'react'
+
+export function useIntersectionObserver(options?: IntersectionObserverInit) {
+  const [isIntersecting, setIsIntersecting] = useState(false)
+  const ref = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    if (!ref.current) return
+    const observer = new IntersectionObserver(([entry]) => setIsIntersecting(entry.isIntersecting), options)
+    observer.observe(ref.current)
+    return () => observer.disconnect()
+  }, [options])
+  return { ref, isIntersecting }
+}
