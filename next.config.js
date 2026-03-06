5426a000d648cf7d401dc489ae9a2b5ffe683b60
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:01:58 +0700

    fix(config): migrate images.domains to remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com'],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+    ],
   },
 }
 
