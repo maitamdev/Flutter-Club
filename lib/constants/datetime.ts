2ccae57edab4763613d0b59cdab7fe918d87d242
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:02:12 +0700

    feat(constants): add date/time constants

---
+++ lib/constants/datetime.ts
@@ -0,0 +1,10 @@
+﻿// Date and time constants
+export const DAYS_OF_WEEK = ['Chu nhat', 'Thu 2', 'Thu 3', 'Thu 4', 'Thu 5', 'Thu 6', 'Thu 7']
+export const MONTHS = ['Thang 1', 'Thang 2', 'Thang 3', 'Thang 4', 'Thang 5', 'Thang 6', 'Thang 7', 'Thang 8', 'Thang 9', 'Thang 10', 'Thang 11', 'Thang 12']
+export const TIME_SLOTS = [
+  '07:00', '07:30', '08:00', '08:30', '09:00', '09:30',
+  '10:00', '10:30', '11:00', '11:30', '12:00', '12:30',
+  '13:00', '13:30', '14:00', '14:30', '15:00', '15:30',
+  '16:00', '16:30', '17:00', '17:30', '18:00', '18:30',
+  '19:00', '19:30', '20:00', '20:30', '21:00',
+]
