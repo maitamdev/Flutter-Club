5aa5542e5e21ae9ecadb8382605bb10073d3474b
maitamgk <maitamit062005@gmail.com>
2026-03-07 02:02:24 +0700

    feat(constants): add social links and contact info

---
+++ lib/constants/contact.ts
@@ -0,0 +1,12 @@
+﻿// Social and contact links
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/ft-club-hub',
+  FACEBOOK: 'https://facebook.com/ftclub.dhv',
+  YOUTUBE: 'https://youtube.com/@ftclub',
+  WEBSITE: 'https://weboom-dhv-tec.vercel.app',
+}
+export const CONTACT_INFO = {
+  EMAIL: 'ftclub@dhv.edu.vn',
+  PHONE: '028-1234-5678',
+  ADDRESS: 'Truong DH Hung Vuong TPHCM, 736 Nguyen Trai, P.11, Q.5',
+}
