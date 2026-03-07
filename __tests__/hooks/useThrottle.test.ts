6f2b07358882796b9acafe436000de16214a327f
maitamgk <maitamit062005@gmail.com>
2026-03-08 00:45:17 +0700

    test(hooks): add unit tests for useThrottle

---
+++ __tests__/hooks/useThrottle.test.ts
@@ -0,0 +1,11 @@
+﻿import { renderHook } from '@testing-library/react';
+import { useThrottle } from '@/lib/hooks/useThrottle';
+
+describe('useThrottle', () => {
+  jest.useFakeTimers();
+
+  it('should return initial value immediately', () => {
+    const { result } = renderHook(() => useThrottle('initial', 500));
+    expect(result.current).toBe('initial');
+  });
+});
