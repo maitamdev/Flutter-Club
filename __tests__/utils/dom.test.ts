656356e3971b31105e9b7bb3791977d4a3f3cb92
maitamgk <maitamit062005@gmail.com>
2026-03-08 00:45:15 +0700

    test(utils): add unit tests for dom utils

---
+++ __tests__/utils/dom.test.ts
@@ -0,0 +1,17 @@
+﻿import { scrollToElement, copyToClipboard, getScrollPosition } from '@/lib/utils/dom';
+
+describe('DOM Utils', () => {
+  describe('getScrollPosition', () => {
+    it('should return scroll position object', () => {
+      const pos = getScrollPosition();
+      expect(pos).toHaveProperty('x');
+      expect(pos).toHaveProperty('y');
+    });
+  });
+
+  describe('scrollToElement', () => {
+    it('should not throw for missing element', () => {
+      expect(() => scrollToElement('nonexistent')).not.toThrow();
+    });
+  });
+});
