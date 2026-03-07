ef2b23cbd5af5decd4f5d8c8db912e2ea90bca7a
maitamgk <maitamit062005@gmail.com>
2026-03-08 00:54:49 +0700

    feat(utils): add debounce utility function

---
+++ lib/utils/debounce.ts
@@ -0,0 +1,6 @@
+﻿export function debounce<T extends (...args: any[]) => any>(fn: T, delay: number): T & { cancel: () => void } {
+  let timer: NodeJS.Timeout;
+  const debounced = (...args: Parameters<T>) => { clearTimeout(timer); timer = setTimeout(() => fn(...args), delay); };
+  debounced.cancel = () => clearTimeout(timer);
+  return debounced as T & { cancel: () => void };
+}
