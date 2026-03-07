722dd8d606f94eed3f300e3b6fd87cf7931f2768
maitamgk <maitamit062005@gmail.com>
2026-03-08 01:09:41 +0700

    feat(layout): add NotificationCenter layout

---
+++ components/layout/notification-center-layout.tsx
@@ -0,0 +1,8 @@
+﻿'use client';
+interface NotificationCenterLayoutProps { children: React.ReactNode; sidebar: React.ReactNode; }
+export function NotificationCenterLayout({ children, sidebar }: NotificationCenterLayoutProps) {
+  return (<div className='flex h-full'>
+    <div className='w-80 border-r overflow-y-auto bg-white dark:bg-gray-900'>{sidebar}</div>
+    <div className='flex-1 overflow-y-auto bg-gray-50 dark:bg-gray-950 p-6'>{children}</div>
+  </div>);
+}
