9ee823a3f1000912e5c440636fdbad02fcc3e421
maitamgk <maitamit062005@gmail.com>
2026-03-08 00:47:12 +0700

    feat(hooks): add useUpdateEffect hook

---
+++ lib/hooks/useUpdateEffect.ts
@@ -0,0 +1,6 @@
+﻿'use client';
+import { useEffect, useRef, DependencyList, EffectCallback } from 'react';
+export function useUpdateEffect(effect: EffectCallback, deps: DependencyList) {
+  const isFirst = useRef(true);
+  useEffect(() => { if (isFirst.current) { isFirst.current = false; return; } return effect(); }, deps);
+}
