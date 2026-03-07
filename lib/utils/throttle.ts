d922d055c5db86d6cb4f0b3b1e3da1e3b3f874d7
maitamgk <maitamit062005@gmail.com>
2026-03-08 00:54:49 +0700

    feat(utils): add throttle utility function

---
+++ lib/utils/throttle.ts
@@ -0,0 +1,6 @@
+﻿export function throttle<T extends (...args: any[]) => any>(fn: T, limit: number): T {
+  let inThrottle = false;
+  return ((...args: Parameters<T>) => {
+    if (!inThrottle) { fn(...args); inThrottle = true; setTimeout(() => (inThrottle = false), limit); }
+  }) as T;
+}
