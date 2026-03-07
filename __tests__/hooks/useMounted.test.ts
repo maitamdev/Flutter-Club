e87a60d22c5c445fff887e221143c39f2b94b824
maitamgk <maitamit062005@gmail.com>
2026-03-08 00:45:17 +0700

    test(hooks): add unit tests for useMounted

---
+++ __tests__/hooks/useMounted.test.ts
@@ -0,0 +1,9 @@
+﻿import { renderHook } from '@testing-library/react';
+import { useMounted } from '@/lib/hooks/useMounted';
+
+describe('useMounted', () => {
+  it('should return true after mount', () => {
+    const { result } = renderHook(() => useMounted());
+    expect(result.current).toBe(true);
+  });
+});
