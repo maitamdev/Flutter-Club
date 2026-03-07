e82cd6d0c2acf96d3a1c9a23c75f1422ff511bff
maitamgk <maitamit062005@gmail.com>
2026-03-08 00:45:17 +0700

    test(hooks): add unit tests for useInterval

---
+++ __tests__/hooks/useInterval.test.ts
@@ -0,0 +1,13 @@
+﻿import { renderHook } from '@testing-library/react';
+import { useInterval } from '@/lib/hooks/useInterval';
+
+describe('useInterval', () => {
+  jest.useFakeTimers();
+
+  it('should call callback at intervals', () => {
+    const callback = jest.fn();
+    renderHook(() => useInterval(callback, 500));
+    jest.advanceTimersByTime(1500);
+    expect(callback).toHaveBeenCalledTimes(3);
+  });
+});
