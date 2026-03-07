f6fd2fe45046d1a70417de6d9a3f098f5e9bfa87
maitamgk <maitamit062005@gmail.com>
2026-03-08 00:55:00 +0700

    feat(constants): add social media link constants

---
+++ lib/constants/social-links.ts
@@ -0,0 +1,8 @@
+﻿export const SOCIAL_LINKS = {
+  FACEBOOK: { url: 'https://facebook.com/flutterclubdhv', label: 'Facebook', icon: 'Facebook', color: '#1877F2' },
+  GITHUB: { url: 'https://github.com/flutterclubdhv', label: 'GitHub', icon: 'Github', color: '#333333' },
+  YOUTUBE: { url: 'https://youtube.com/@flutterclubdhv', label: 'YouTube', icon: 'Youtube', color: '#FF0000' },
+  DISCORD: { url: 'https://discord.gg/flutterdhv', label: 'Discord', icon: 'MessageCircle', color: '#5865F2' },
+  WEBSITE: { url: 'https://flutterclub.dhv.edu.vn', label: 'Website', icon: 'Globe', color: '#3B82F6' },
+} as const;
+export const CONTACT_INFO = { email: 'flutterclub@dhv.edu.vn', phone: '028-1234-5678', address: 'ÄH HÃ¹ng VÆ°Æ¡ng TPHCM' };
