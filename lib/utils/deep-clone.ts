a044638249cda43f3704bc5201334c2c7f372fdc
maitamgk <maitamit062005@gmail.com>
2026-03-08 00:54:49 +0700

    feat(utils): add deep clone utility

---
+++ lib/utils/deep-clone.ts
@@ -0,0 +1,8 @@
+﻿export function deepClone<T>(obj: T): T {
+  if (obj === null || typeof obj !== 'object') return obj;
+  if (obj instanceof Date) return new Date(obj.getTime()) as unknown as T;
+  if (obj instanceof Array) return obj.map(item => deepClone(item)) as unknown as T;
+  const cloned = {} as T;
+  for (const key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) (cloned as any)[key] = deepClone((obj as any)[key]); }
+  return cloned;
+}
