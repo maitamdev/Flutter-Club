455804bd826bc91bb30bc72c1cb557b8a24d5237
maitamgk <maitamit062005@gmail.com>
2026-03-08 00:45:14 +0700

    test(utils): add unit tests for object utils

---
+++ __tests__/utils/object.test.ts
@@ -0,0 +1,33 @@
+﻿import { pick, omit, isEmpty, deepEqual, flattenObject } from '@/lib/utils/object';
+
+describe('Object Utils', () => {
+  describe('pick', () => {
+    it('should pick specified keys', () => {
+      const obj = { a: 1, b: 2, c: 3 };
+      expect(pick(obj, ['a', 'c'])).toEqual({ a: 1, c: 3 });
+    });
+  });
+
+  describe('omit', () => {
+    it('should omit specified keys', () => {
+      const obj = { a: 1, b: 2, c: 3 };
+      expect(omit(obj, ['b'])).toEqual({ a: 1, c: 3 });
+    });
+  });
+
+  describe('isEmpty', () => {
+    it('should detect empty object', () => {
+      expect(isEmpty({})).toBe(true);
+    });
+    it('should detect non-empty object', () => {
+      expect(isEmpty({ a: 1 })).toBe(false);
+    });
+  });
+
+  describe('deepEqual', () => {
+    it('should compare objects deeply', () => {
+      expect(deepEqual({ a: { b: 1 } }, { a: { b: 1 } })).toBe(true);
+      expect(deepEqual({ a: 1 }, { a: 2 })).toBe(false);
+    });
+  });
+});
