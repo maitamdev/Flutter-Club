2a8fc58a9c88c5ea49450bf832af467b230b5130
maitamgk <maitamit062005@gmail.com>
2026-03-08 00:54:49 +0700

    feat(utils): add random ID generator

---
+++ lib/utils/random-id.ts
@@ -0,0 +1,6 @@
+﻿export function randomId(length = 12): string {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  return Array.from({ length }, () => chars[Math.floor(Math.random() * chars.length)]).join('');
+}
+export function nanoid(size = 21): string { return crypto.getRandomValues(new Uint8Array(size)).reduce((id, byte) => id + (byte & 63).toString(36), ''); }
+export function prefixedId(prefix: string): string { return `${prefix}_${randomId(8)}_${Date.now().toString(36)}`; }
