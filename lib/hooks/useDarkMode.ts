a8f99b57c7883f75a17debae03c98a568953cef0
maitamgk <maitamit062005@gmail.com>
2026-03-08 00:47:12 +0700

    feat(hooks): add useDarkMode hook

---
+++ lib/hooks/useDarkMode.ts
@@ -0,0 +1,11 @@
+﻿'use client';
+import { useTheme } from 'next-themes';
+import { useCallback } from 'react';
+export function useDarkMode() {
+  const { theme, setTheme, resolvedTheme } = useTheme();
+  const isDarkMode = resolvedTheme === 'dark';
+  const toggle = useCallback(() => setTheme(isDarkMode ? 'light' : 'dark'), [isDarkMode, setTheme]);
+  const enable = useCallback(() => setTheme('dark'), [setTheme]);
+  const disable = useCallback(() => setTheme('light'), [setTheme]);
+  return { isDarkMode, toggle, enable, disable, theme };
+}
