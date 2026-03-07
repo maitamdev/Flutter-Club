a169d18c0bfc040800e37dd8e460f5bb27436af2
maitamgk <maitamit062005@gmail.com>
2026-03-08 00:45:17 +0700

    test(hooks): add unit tests for useTimeout

---
+++ __tests__/hooks/useTimeout.test.ts
@@ -0,0 +1,14 @@
+﻿import { renderHook } from '@testing-library/react';
+import { useTimeout } from '@/lib/hooks/useTimeout';
+
+describe('useTimeout', () => {
+  jest.useFakeTimers();
+
+  it('should call callback after delay', () => {
+    const callback = jest.fn();
+    renderHook(() => useTimeout(callback, 1000));
+    expect(callback).not.toHaveBeenCalled();
+    jest.advanceTimersByTime(1000);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+});
