ec0fa67b30789904108de3a108a40f7c0c974fc6
maitamgk <maitamit062005@gmail.com>
2026-03-08 00:54:57 +0700

    feat(constants): add feature flags

---
+++ lib/constants/feature-flags.ts
@@ -0,0 +1,9 @@
+﻿export const FEATURE_FLAGS = {
+  ENABLE_AI_ASSISTANT: 'enable_ai_assistant', ENABLE_DARK_MODE: 'enable_dark_mode',
+  ENABLE_REAL_TIME_CHAT: 'enable_real_time_chat', ENABLE_PDF_EXPORT: 'enable_pdf_export',
+  ENABLE_QR_CHECKIN: 'enable_qr_checkin', ENABLE_GAMIFICATION: 'enable_gamification',
+  ENABLE_ADVANCED_ANALYTICS: 'enable_advanced_analytics', ENABLE_NOTIFICATIONS: 'enable_notifications',
+  ENABLE_FILE_SHARING: 'enable_file_sharing', ENABLE_VIDEO_CALL: 'enable_video_call',
+  ENABLE_CERTIFICATE_GEN: 'enable_certificate_gen', ENABLE_BULK_IMPORT: 'enable_bulk_import',
+} as const;
+export type FeatureFlag = (typeof FEATURE_FLAGS)[keyof typeof FEATURE_FLAGS];
