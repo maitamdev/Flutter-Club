3b559d494975ad665d21140b2bdb453cdceb4edb
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:49:28 +0700

    feat(hooks): add useDebounceCallback hook

---
+++ lib/hooks/useDebounceCallback.ts
@@ -0,0 +1,9 @@
+﻿'use client';
+import { useCallback, useRef } from 'react';
+export function useDebounceCallback<T extends (...args: any[]) => void>(callback: T, delay: number = 300): T {
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  return useCallback((...args: any[]) => {
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => callback(...args), delay);
+  }, [callback, delay]) as T;
+}
