b02467b01f3021c01bc312eb62a9f4bf31e887f8
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:33:07 +0700

    test(hooks): add useMap unit tests

---
+++ __tests__/hooks/useMap.test.ts
@@ -0,0 +1,24 @@
+﻿describe('useMap', () => {
+  it('should initialize with empty map', () => {
+    const map = new Map();
+    expect(map.size).toBe(0);
+  });
+
+  it('should set values', () => {
+    const map = new Map();
+    map.set('key', 'value');
+    expect(map.get('key')).toBe('value');
+  });
+
+  it('should delete values', () => {
+    const map = new Map([['key', 'value']]);
+    map.delete('key');
+    expect(map.has('key')).toBe(false);
+  });
+
+  it('should reset map', () => {
+    const map = new Map([['a', 1], ['b', 2]]);
+    map.clear();
+    expect(map.size).toBe(0);
+  });
+});
