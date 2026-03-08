320eba7a545a22e5f46ed31b455c765f5e6076e5
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:45:32 +0700

    feat(types): add advanced TypeScript utility types

---
+++ types/utility-types.ts
@@ -0,0 +1,9 @@
+﻿// Advanced utility types
+export type DeepPartial<T> = { [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P]; };
+export type DeepReadonly<T> = { readonly [P in keyof T]: T[P] extends object ? DeepReadonly<T[P]> : T[P]; };
+export type Nullable<T> = T | null;
+export type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+export type RequiredKeys<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
+export type ValueOf<T> = T[keyof T];
+export type Entries<T> = { [K in keyof T]: [K, T[K]] }[keyof T][];
+export type AsyncReturnType<T extends (...args: unknown[]) => Promise<unknown>> = T extends (...args: unknown[]) => Promise<infer R> ? R : never;
