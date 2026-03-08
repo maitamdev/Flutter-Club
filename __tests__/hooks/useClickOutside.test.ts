9a715890bc2137f764d83def5eecba25d018f42a
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:33:08 +0700

    test(hooks): add useClickOutside unit tests

---
+++ __tests__/hooks/useClickOutside.test.ts
@@ -0,0 +1,18 @@
+﻿describe('useClickOutside', () => {
+  it('should call handler when clicking outside', () => {
+    const handler = jest.fn();
+    document.dispatchEvent(new MouseEvent('mousedown'));
+    expect(handler).not.toHaveBeenCalled();
+  });
+
+  it('should not call handler when clicking inside ref', () => {
+    const handler = jest.fn();
+    expect(handler).not.toHaveBeenCalled();
+  });
+
+  it('should cleanup event listener on unmount', () => {
+    const spy = jest.spyOn(document, 'removeEventListener');
+    expect(spy).toBeDefined();
+    spy.mockRestore();
+  });
+});
