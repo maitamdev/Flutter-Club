2a7d6d21d431d8c4606eb13bcaa29cd4c23be0fc
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:35:32 +0700

    feat(i18n): add polls module translations

---
+++ lib/i18n/modules/polls.ts
@@ -0,0 +1,3 @@
+﻿// polls module translations
+export const pollsVi: Record<string, string> = { 'polls.title': 'Binh chon', 'polls.create': 'Tao binh chon', 'polls.vote': 'Bo phieu', 'polls.results': 'Ket qua', 'polls.closed': 'Da dong' };
+export const pollsEn: Record<string, string> = { 'polls.title': 'Polls', 'polls.create': 'Create Poll', 'polls.vote': 'Vote', 'polls.results': 'Results', 'polls.closed': 'Closed' };
