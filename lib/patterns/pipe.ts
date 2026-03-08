aaac88636bdbd06ea1f615a48ca5db295794acbc
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:45:29 +0700

    refactor: add pipe utility for function composition

---
+++ lib/patterns/pipe.ts
@@ -0,0 +1,4 @@
+﻿// Pipe utility for function composition
+export function pipe<T>(...fns: ((value: T) => T)[]): (value: T) => T { return (value: T) => fns.reduce((acc, fn) => fn(acc), value); }
+export function pipeAsync<T>(...fns: ((value: T) => Promise<T>)[]): (value: T) => Promise<T> { return (value: T) => fns.reduce(async (acc, fn) => fn(await acc), Promise.resolve(value)); }
+export function compose<T>(...fns: ((value: T) => T)[]): (value: T) => T { return pipe(...fns.reverse()); }
