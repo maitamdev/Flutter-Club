7a80d1f3fa9ec29a7d4c46132c7a7c76497e2bad
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:49:30 +0700

    feat(hooks): add useForceUpdate hook

---
+++ lib/hooks/useForceUpdate.ts
@@ -0,0 +1,6 @@
+﻿'use client';
+import { useState, useCallback } from 'react';
+export function useForceUpdate() {
+  const [, setState] = useState(0);
+  return useCallback(() => setState(s => s + 1), []);
+}
