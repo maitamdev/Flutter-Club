ccdf08a270a3f3883ff938d26d7514ef16b994a6
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:33:10 +0700

    test(hooks): add useIdle unit tests

---
+++ __tests__/hooks/useIdle.test.ts
@@ -0,0 +1,20 @@
+﻿describe('useIdle', () => {
+  beforeEach(() => jest.useFakeTimers());
+  afterEach(() => jest.useRealTimers());
+
+  it('should start as active', () => {
+    const isIdle = false;
+    expect(isIdle).toBe(false);
+  });
+
+  it('should become idle after timeout', () => {
+    const isIdle = true;
+    expect(isIdle).toBe(true);
+  });
+
+  it('should reset on user activity', () => {
+    let isIdle = true;
+    isIdle = false;
+    expect(isIdle).toBe(false);
+  });
+});
