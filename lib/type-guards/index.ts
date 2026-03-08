2af8eeb14a38d1ec1e409b70f96d38e6f5f8ed78
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:35:34 +0700

    feat(types): add index type guards

---
+++ lib/type-guards/index.ts
@@ -0,0 +1,11 @@
+﻿// Type guard barrel export
+export * from './event';
+export * from './member';
+export * from './quiz';
+export * from './assignment';
+export * from './session';
+export * from './notification';
+export * from './project';
+export * from './material';
+export * from './feedback';
+export * from './common';
