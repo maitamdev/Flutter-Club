de16e313cc5487ed6b3eb80164f1c59ac9130a0f
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:49:31 +0700

    feat(hooks): add useWhyDidYouUpdate hook

---
+++ lib/hooks/useWhyDidYouUpdate.ts
@@ -0,0 +1,18 @@
+﻿'use client';
+import { useEffect, useRef } from 'react';
+export function useWhyDidYouUpdate<T extends Record<string, unknown>>(name: string, props: T): void {
+  const previousProps = useRef<T>();
+  useEffect(() => {
+    if (previousProps.current) {
+      const allKeys = Object.keys({ ...previousProps.current, ...props });
+      const changes: Record<string, { from: unknown; to: unknown }> = {};
+      allKeys.forEach(key => {
+        if (previousProps.current![key] !== props[key]) {
+          changes[key] = { from: previousProps.current![key], to: props[key] };
+        }
+      });
+      if (Object.keys(changes).length) console.log('[why-update]', name, changes);
+    }
+    previousProps.current = props;
+  });
+}
