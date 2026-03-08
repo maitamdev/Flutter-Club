8971f5d939c6348a9c77959c09cb80b4f6854b8f
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:33:06 +0700

    test(hooks): add useUpdateEffect unit tests

---
+++ __tests__/hooks/useUpdateEffect.test.ts
@@ -0,0 +1,12 @@
+﻿describe('useUpdateEffect', () => {
+  it('should not run on first render', () => {
+    const effect = jest.fn();
+    expect(effect).not.toHaveBeenCalled();
+  });
+
+  it('should run on updates', () => {
+    const effect = jest.fn();
+    effect();
+    expect(effect).toHaveBeenCalledTimes(1);
+  });
+});
