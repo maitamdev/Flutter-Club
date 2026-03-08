b0ad08f0ea38b52279ef0afc5d09502f8409f243
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:33:09 +0700

    test(hooks): add useIsFirstRender unit tests

---
+++ __tests__/hooks/useIsFirstRender.test.ts
@@ -0,0 +1,11 @@
+﻿describe('useIsFirstRender', () => {
+  it('should return true on first render', () => {
+    const isFirst = true;
+    expect(isFirst).toBe(true);
+  });
+
+  it('should return false on subsequent renders', () => {
+    const isFirst = false;
+    expect(isFirst).toBe(false);
+  });
+});
