b5e59da55658b8f416a2962d51bb0ba92801ebaa
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:49:29 +0700

    feat(hooks): add useClickAway hook

---
+++ lib/hooks/useClickAway.ts
@@ -0,0 +1,13 @@
+﻿'use client';
+import { useEffect, useRef } from 'react';
+export function useClickAway<T extends HTMLElement>(onClickAway: () => void) {
+  const ref = useRef<T>(null);
+  useEffect(() => {
+    const handler = (event: MouseEvent | TouchEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) onClickAway();
+    };
+    document.addEventListener('mousedown', handler);
+    return () => document.removeEventListener('mousedown', handler);
+  }, [onClickAway]);
+  return ref;
+}
