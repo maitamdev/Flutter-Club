c499d8f712744b672dc94034355167f4f689ddd8
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:45:28 +0700

    refactor: add factory pattern implementation

---
+++ lib/patterns/factory.ts
@@ -0,0 +1,3 @@
+﻿// Factory pattern
+export type Creator<T> = (...args: unknown[]) => T;
+export class Factory<T> { private creators = new Map<string, Creator<T>>(); register(type: string, creator: Creator<T>): void { this.creators.set(type, creator); } create(type: string, ...args: unknown[]): T { const creator = this.creators.get(type); if (!creator) throw new Error('Unknown type: ' + type); return creator(...args); } getTypes(): string[] { return Array.from(this.creators.keys()); } }
