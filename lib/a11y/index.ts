6a2b1c55be2cc2b8f80ae3b9f539211993ba805c
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:45:25 +0700

    feat(a11y): add accessibility module barrel export

---
+++ lib/a11y/index.ts
@@ -0,0 +1,6 @@
+﻿export * from './focus-management';
+export * from './screen-reader';
+export * from './keyboard-nav';
+export * from './aria-helpers';
+export * from './color-contrast';
+export * from './announcer';
