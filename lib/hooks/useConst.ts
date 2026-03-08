504006ecca29cb134e1f176cd3d326073d2ed3df
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:49:30 +0700

    feat(hooks): add useConst hook

---
+++ lib/hooks/useConst.ts
@@ -0,0 +1,9 @@
+﻿'use client';
+import { useRef } from 'react';
+export function useConst<T>(initializer: T | (() => T)): T {
+  const ref = useRef<{ value: T }>();
+  if (ref.current === undefined) {
+    ref.current = { value: typeof initializer === 'function' ? (initializer as () => T)() : initializer };
+  }
+  return ref.current.value;
+}
