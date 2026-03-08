9d5e07f5c76273b4eada7371572f08b03d274481
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:45:28 +0700

    refactor: add singleton pattern implementation

---
+++ lib/patterns/singleton.ts
@@ -0,0 +1,2 @@
+﻿// Singleton pattern
+export function createSingleton<T>(factory: () => T): () => T { let instance: T | null = null; return () => { if (instance === null) instance = factory(); return instance; }; }
