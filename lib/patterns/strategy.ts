2ea91568ff01c971d70cf2a2156f98950df39b3e
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:45:28 +0700

    refactor: add strategy pattern implementation

---
+++ lib/patterns/strategy.ts
@@ -0,0 +1,3 @@
+﻿// Strategy pattern
+export interface Strategy<TInput, TOutput> { execute(input: TInput): TOutput; }
+export class StrategyContext<TInput, TOutput> { private strategy: Strategy<TInput, TOutput>; constructor(strategy: Strategy<TInput, TOutput>) { this.strategy = strategy; } setStrategy(strategy: Strategy<TInput, TOutput>): void { this.strategy = strategy; } execute(input: TInput): TOutput { return this.strategy.execute(input); } }
