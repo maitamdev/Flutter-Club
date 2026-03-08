17bfbb1cf3b527e63e8461c3b7cb80aa6b02c244
maitamgk <maitamit062005@gmail.com>
2026-03-09 02:49:30 +0700

    feat(hooks): add useLatest hook

---
+++ lib/hooks/useLatest.ts
@@ -0,0 +1,7 @@
+﻿'use client';
+import { useRef, useEffect } from 'react';
+export function useLatest<T>(value: T) {
+  const ref = useRef(value);
+  useEffect(() => { ref.current = value; });
+  return ref;
+}
